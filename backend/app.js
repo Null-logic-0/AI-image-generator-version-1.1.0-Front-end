c62455f860350c5184b9e6c76fb16b6a9558c42e
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-02-28 12:02:02 +0400

    Emplement security

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss";
 
 import { router as userRouter } from "./routes/userRoutes.js";
 import { globalErrorHandler } from "./controllers/errorController.js";
@@ -15,13 +18,45 @@ app.use(cors());
 app.options("*", cors());
 app.use(helmet());
 
-// Middlewares
+// Global Middlewares
 if ((process.env.NODE_ENV = "development")) {
   app.use(morgan("dev"));
   console.log(`App is on ${process.env.NODE_ENV} mode`);
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP,please try again in an hour!",
+});
+
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+// XSS Middleware
+const xssSanitization = (req, res, next) => {
+  if (req.body) {
+    Object.keys(req.body).forEach((key) => {
+      req.body[key] = xss(req.body[key]);
+    });
+  }
+  if (req.query) {
+    Object.keys(req.query).forEach((key) => {
+      req.query[key] = xss(req.query[key]);
+    });
+  }
+  if (req.params) {
+    Object.keys(req.params).forEach((key) => {
+      req.params[key] = xss(req.params[key]);
+    });
+  }
+  next();
+};
+
+app.use(xssSanitization);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
