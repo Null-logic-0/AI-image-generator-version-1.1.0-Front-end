0c6df718d2b876a2c5d7e97bef2a4abbe905d77c
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-03-05 13:14:15 +0400

    Created not-found and error page

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,21 @@
 "use client";
+import Image from "next/image";
 
 function error() {
-  return <div>erro</div>;
+  return (
+    <div>
+      <Image
+        src="/erorr-img.jpg"
+        alt="error-image"
+        fill
+        className="object-cover"
+      />
+      <span className="fixed z-50 text-black font-semibold text-xl bottom-5 w-full text-center">
+        Ooops... <br />
+        Something went wrong
+      </span>
+    </div>
+  );
 }
 
 export default error;
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,11 +4,16 @@ import notFoundImage from "../../public/not-found.png";
 function NotFound() {
   return (
     <div className="mx-auto w-full justify-center flex items-center min-h-screen">
-      <div className="flex  flex-col gap-8">
-        <Image src={notFoundImage} alt="icon" className="object-fill" />
-        <h1 className="text-center text-2xl text-gray-400 leading-10">
-          404 ERORR ⚠️
-          <br />
+      <div className="flex  flex-col gap-2 p-4">
+        <Image
+          src={"/not-found.png"}
+          alt="icon"
+          className="object-fill"
+          width={350}
+          quality={100}
+          height={350}
+        />
+        <h1 className="text-center text-3xl text-gray-400 leading-10">
           Page Not Found
         </h1>
       </div>
