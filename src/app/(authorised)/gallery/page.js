1af25fe60dd8a857029fce135228e78fe7e1fbc9
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-03-06 16:11:34 +0400

    fix bugs

--- src/app/(authorised)/gallery/page.js
+++ src/app/(authorised)/gallery/page.js
@@ -1,44 +1,14 @@
-import UserImage from "@/components/Gallery/UserImage.js";
-import Image from "next/image";
-import emptyImg from "../../../../public/empty.png";
-import { getUserImages } from "@/lib/data-services.js";
+import { Suspense } from "react";
+import Spinner from "@/components/Spinner";
+import ImageGallery from "@/components/Gallery/ImageGallery";
 
 async function GalleryPage() {
-  const { success, data, message } = await getUserImages();
-
-  if (!success) {
-    return (
-      <div className="flex flex-col items-center gap-2 justify-center mx-auto">
-        <span className="text-center text-xl text-gray-400">{message}</span>
-      </div>
-    );
-  }
-  if (!success) {
-    return (
-      <div className="flex flex-col items-center gap-2 justify-center">
-        <span className="text-center text-xl text-gray-400">{message}</span>
-      </div>
-    );
-  }
   return (
     <div className="mx-auto w-full flex flex-col gap-12 px-4">
       <h1 className="text-center font-bold text-white text-xl">Gallery</h1>
-      <ul className="flex justify-center items-center ">
-        <li className="flex gap-4 items-center flex-wrap justify-center">
-          {data.length > 0 ? (
-            data.map((image) => (
-              <UserImage key={image._id} src={image.imageData} id={image._id} />
-            ))
-          ) : (
-            <div className="flex flex-col items-center gap-2 justify-center">
-              <Image src={emptyImg} alt="icon" width={80} height={80} />
-              <span className="text-center text-xl text-gray-400">
-                No images
-              </span>
-            </div>
-          )}
-        </li>
-      </ul>
+      <Suspense fallback={<Spinner />}>
+        <ImageGallery />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Gallery/ImageGallery.js
@@ -0,0 +1,35 @@
+import { getUserImages } from "@/lib/data-services.js";
+
+import Image from "next/image";
+import UserImage from "./UserImage";
+import emptyImg from "../../../public/empty.png";
+
+async function ImageGallery() {
+  const { success, data, message } = await getUserImages();
+
+  if (!success) {
+    return (
+      <div className="flex flex-col items-center gap-2 justify-center mx-auto">
+        <span className="text-center text-xl text-gray-400">{message}</span>
+      </div>
+    );
+  }
+  return (
+    <ul className="flex justify-center items-center ">
+      <li className="flex gap-4 items-center flex-wrap justify-center">
+        {data.length > 0 ? (
+          data.map((image) => (
+            <UserImage key={image._id} src={image.imageData} id={image._id} />
+          ))
+        ) : (
+          <div className="flex flex-col items-center gap-2 justify-center">
+            <Image src={emptyImg} alt="icon" width={80} height={80} />
+            <span className="text-center text-xl text-gray-400">No images</span>
+          </div>
+        )}
+      </li>
+    </ul>
+  );
+}
+
+export default ImageGallery;
